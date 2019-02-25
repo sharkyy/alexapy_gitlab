1364ca63a0d487d2216c163811426a093383dc3e
Alan D. Tse <alandtse@gmail.com>
2019-02-25 00:03:16 -0800

    Fix pylint errors

--- alexapy/__init__.py
+++ alexapy/__init__.py
@@ -1,3 +1,14 @@
-"""Define module-level imports."""
-from .alexalogin import AlexaLogin  # noqa
-from .alexaapi import AlexaAPI  # noqa
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#  SPDX-License-Identifier: Apache-2.0
+"""
+Python Package for controlling Alexa devices (echo dot, etc) programmatically.
+
+For more details about this api, please refer to the documentation at
+https://gitlab.com/keatontaylor/alexapy
+"""
+from .alexalogin import AlexaLogin
+from .alexaapi import AlexaAPI
+from .__version__ import __version__
+
+__all__ = ['AlexaLogin', 'AlexaAPI', '__version__']
